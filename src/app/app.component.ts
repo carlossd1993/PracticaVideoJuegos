9508c36a005b9edb865e34845e99bf94f6caddcc
carlossd1993 <161085364+carlossd1993@users.noreply.github.com>
2024-03-20 13:53:53 +0100

    Create the navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {MaterialModuleModule} from "./modules/material-module/material-module.module";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MaterialModuleModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'PracticaVideoJuegos';
+  mostrarMenu=false;
+  toggleMenu(){
+    if(this.mostrarMenu){
+      this.mostrarMenu=false;
+    }else {
+      this.mostrarMenu=true
+    }
+    console.log(this.mostrarMenu)
+  }
 }
