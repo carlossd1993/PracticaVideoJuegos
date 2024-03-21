a7d4457ca1111c78dd0d0fa95c5160012987779f
carlossd1993 <161085364+carlossd1993@users.noreply.github.com>
2024-03-21 23:23:42 +0100

    Añadidas consultas a la api para los juegos por categorías

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
 import {JuegoComponent} from "./pages/juego/juego.component";
+import {CategoriasComponent} from "./pages/categorias/categorias.component";
 
 export const routes: Routes = [
   {
@@ -8,8 +9,53 @@ export const routes: Routes = [
     component: HomeComponent
   },
   {
+    path:"mmorpg",
+    component: CategoriasComponent
+  },
+  {
+    path:"shooter",
+    component: CategoriasComponent
+  },
+  {
+    path:"moba",
+    component: CategoriasComponent
+  },
+  {
+    path:"anime",
+    component: CategoriasComponent
+  },
+
+  {
+    path:"strategy",
+    component: CategoriasComponent
+  },
+  {
+    path:"fantasy",
+    component: CategoriasComponent
+  },
+
+
+  {
+    path:"racing",
+    component: CategoriasComponent
+  },
+  {
+    path:"fighting",
+    component: CategoriasComponent
+  },
+  {
+    path:"social",
+    component: CategoriasComponent
+  },
+  {
+    path:"sports",
+    component: CategoriasComponent
+  },
+
+  {
     path:"**",
     component: JuegoComponent
-  }
+  },
+
 
 ];
